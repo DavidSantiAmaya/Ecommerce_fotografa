46d94e9a4eba4bd575f8d0ec3043bb18722fde8c
Dankel26 <darodrigueza@uniboyaca.edu.co>
2025-05-12 11:32:13 -0500

    Update product.service.spec.ts

--- src/app/services/product.service.spec.ts
+++ src/app/services/product.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
-import { ProductService } from './product.service';
+import { ProfesionalService } from './product.service';
 
 
 describe('ProductService', () => {
-  let service: ProductService;
+  let service: ProfesionalService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(ProductService);
+    service = TestBed.inject(ProfesionalService);
   });
 
   it('should be created', () => {
